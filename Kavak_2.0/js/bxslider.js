86f0ead72b277a476bb550d7919203b62e386611
Weneceslao Trejo Caamal <55564740+WencesTRC@users.noreply.github.com>
2021-03-26 12:28:16 -0500

    BxSlider
    Inicializa el BxSlider

---
+++ Kavak_2.0/js/bxslider.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.bxslider').bxSlider({
+          mode:'fade',
+          captions:true,
+          auto: true,
+          autoControls: true,
+          stopAutoOnClick: true,
+          pager: true,
+          slideWidth: 700
+    });
+  });
